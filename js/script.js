56670754d6f68a553cecfa03140e95972123270f
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-07-17 10:42:51 +0800

    burger toggle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+
+let menunavburgercontainer = document.getElementsByClassName('menunavburgercontainer');
+let menuburgericon = document.getElementsByClassName('menuburger');
+let menuburgercontainer = document.getElementsByClassName('menuburgercontainer');
+
+function togglenavburger(){
+    menunavburgercontainer[0].classList.toggle("none");
+}
+
+document.getElementsByClassName('menuburgercontainer')[0].addEventListener("click", togglenavburger)   
