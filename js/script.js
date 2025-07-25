6601fa580f9bcb76d053ea91ac3a9ae4b79dfa92
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-07-25 23:45:12 +0800

    contact us banner

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let menuburgericon = document.getElementsByClassName('menuburger');
 let menuburgercontainer = document.getElementsByClassName('menuburgercontainer');
 
 function togglenavburger(){
-    menunavburgercontainer[0].classList.toggle("none");
+    menunavburgercontainer[0].classList.toggle("hidden");
 }
 
 document.getElementsByClassName('menuburgercontainer')[0].addEventListener("click", togglenavburger)   
